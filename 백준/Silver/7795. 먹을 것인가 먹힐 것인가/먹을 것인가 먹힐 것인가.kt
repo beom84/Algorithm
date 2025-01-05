877c7c3e19c7f700776b1ecd258f2a2a591a0610
beom84 <cjja00@konkuk.ac.kr>
2025-01-05 13:37:06 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 1452 ms, Memory: 57428 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.kt
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.kt
@@ -1,17 +1,28 @@
 fun main() = with(System.`in`.bufferedReader()) {
+    val sb = StringBuilder()
     repeat(readLine().toInt()) {
-        val (N, M) = readLine().split(" ").map { it.toInt() }
-        val aType = readLine().split(" ").map { it.toInt() }
-        var bType = readLine().split(" ").map { it.toInt() }
+        val (N, M) = readLine().split(" ").map ( String::toInt )
+        val aType = readLine().split(" ").map ( String::toInt ).sorted()
+        val bType = readLine().split(" ").map ( String::toInt ).sorted()
         var answer = 0
-        bType = bType.sortedDescending()
-        aType.forEach { A ->
-            for (i in bType.indices) {
-                if (A > bType[i]) {
-                    answer += bType.size - i
-                    break
+        for (a in aType) {
+            var (l, r) = 0 to M - 1
+            var target = -1
+            while (l <= r) {
+                val m = (l + r).div(2)
+
+                if (bType[m] <= a) {
+                    l = m + 1
+                    target = m
+                } else {
+                    r = m - 1
                 }
             }
+
+            while (target >= 0 && bType[target] == a) {
+                target--
+            }
+            answer += target + 1
         }
         println(answer)
     }
