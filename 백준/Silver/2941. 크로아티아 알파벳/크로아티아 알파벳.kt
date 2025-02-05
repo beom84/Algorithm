e991f96fc61192d948660b3e8107abdc43168166
beom84 <cjja00@konkuk.ac.kr>
2025-02-05 19:43:20 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 80 ms, Memory: 12276 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.kt
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.kt
@@ -2,78 +2,25 @@ fun main() = with(System.`in`.bufferedReader()) {
     var count = 0
     var index = 0
     val str = readLine()
-    val changedList = listOf('c', 'd', 'l', 'n', 's', 'z')
-
+    
     while (index < str.length) {
-        if (changedList.contains(str[index]) && index <= str.length - 2) {
+        if ( index <= str.length - 2) {
             when (str[index]) {
-                'c' -> {
-                    if (str[index + 1] == '=' || str[index + 1] == '-') {
-                        count++
-                        index += 2
-                    }else {
-                        index++
-                        count++
-                    }
-                }
+                'c' -> if (str[index + 1] == '=' || str[index + 1] == '-') index++
 
-                'd' -> {
-                    if (str[index + 1] == '-') {
-                        count++
-                        index += 2
-                    } else if (index <= str.length - 3 && str[index + 1] == 'z' && str[index + 2] == '=') {
-                        count++
-                        index += 3
-                    }else {
-                        index++
-                        count++
-                    }
-                }
+                'd' -> if (str[index + 1] == '-') index++ else if (index <= str.length - 3 && str[index + 1] == 'z' && str[index + 2] == '=') index += 2
 
-                'l' -> {
-                    if (str[index + 1] == 'j') {
-                        count++
-                        index += 2
-                    }else {
-                        index++
-                        count++
-                    }
-                }
+                'l' -> if (str[index + 1] == 'j') index++
 
-                'n' -> {
-                    if (str[index + 1] == 'j') {
-                        count++
-                        index += 2
-                    }else {
-                        index++
-                        count++
-                    }
-                }
+                'n' -> if (str[index + 1] == 'j') index++
 
-                's' -> {
-                    if (str[index + 1] == '=') {
-                        count++
-                        index += 2
-                    }else {
-                        index++
-                        count++
-                    }
-                }
+                's' -> if (str[index + 1] == '=') index++
 
-                'z' -> {
-                    if (str[index + 1] == '=') {
-                        count++
-                        index += 2
-                    }else {
-                        index++
-                        count++
-                    }
-                }
+                'z' -> if (str[index + 1] == '=') index++
             }
-        } else {
-            index++
-            count++
         }
+        index++
+        count++
     }
 
     println(count)
