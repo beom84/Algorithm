d5da3d8e4cb3b0f210e1fb10f79f1a8ecc6ca428
beom84 <cjja00@konkuk.ac.kr>
2025-02-11 15:01:54 +0900

    [Silver II] Title: 좌표 압축, Time: 2756 ms, Memory: 364064 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.kt
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.kt
@@ -1,55 +1,16 @@
 fun main() = with(System.`in`.bufferedReader()) {
     val n = readLine().toInt()
-    val xCoordinates = Array(n) { 0 to 0 }
-    val answer = Array(n) { 0 to 0 }
-    val temp = Array(n) { 0 to 0 }
     val sb = StringBuilder()
-
-    readLine().split(" ").mapIndexed { idx, x ->
-        xCoordinates[idx] = Pair(x.toInt(), idx)
-    }
-
-
-    mergeSorting(xCoordinates, temp, 0, xCoordinates.lastIndex)
-
+    val xCoordinates = readLine().split(" ").map { it.toInt() }
+    val sortedArr = xCoordinates.distinct().sorted()
+    val hasMap = HashMap<Int,Int>()
     var count = 0
-    answer[0] = Pair(0, xCoordinates[0].second)
-    for (i in 1..xCoordinates.lastIndex) {
-        if (xCoordinates[i].first != xCoordinates[i - 1].first) count++
-        answer[i] = Pair(count, xCoordinates[i].second)
+    
+    sortedArr.forEach { 
+        hasMap[it] = count++
     }
+    
+    for(i in 0 until n) sb.append("${hasMap[xCoordinates[i]]} ")
 
-    answer.sortWith(compareBy { it.second })
-
-    for (i in answer) sb.append("${i.first} ")
-
-    println(sb.toString())
-}
-
-fun mergeSorting(array: Array<Pair<Int, Int>>, temp: Array<Pair<Int, Int>>, start: Int, end: Int) {
-    val mid = (start + end) / 2
-
-    if (start < end) {
-        mergeSorting(array, temp, start, mid)
-        mergeSorting(array, temp, mid + 1, end)
-        merging(array, temp, start, mid + 1, end)
-    }
-}
-
-fun merging(arr: Array<Pair<Int, Int>>, temp: Array<Pair<Int, Int>>, start: Int, mid: Int, end: Int) {
-    var tempPos = start
-    var left = start
-    var right = mid
-    while (left <= mid - 1 && right <= end) {
-        if (arr[left].first <= arr[right].first) temp[tempPos] = arr[left++]
-        else temp[tempPos] = arr[right++]
-
-        tempPos++
-    }
-
-    while (left <= mid - 1) temp[tempPos++] = arr[left++]
-
-    while (right <= end) temp[tempPos++] = arr[right++]
-
-    for (i in start..end) arr[i] = temp[i]
+    println(sb)
 }
