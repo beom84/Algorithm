32bc47e14c414c1daa885a79505a3bd4df55dee6
beom84 <cjja00@konkuk.ac.kr>
2025-02-11 13:40:06 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 2068 ms, Memory: 107088 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.kt
@@ -0,0 +1,51 @@
+fun main() = with(System.`in`.bufferedReader()) {
+    val N = readLine().toInt()
+    val location = Array(N) {
+        val (x, y) = readLine().split(" ").map { it.toInt() }
+        x to y
+    }
+
+    val temp = Array(N) { 0 to 0 }
+
+    mergeSort(location, temp, 0, location.lastIndex)
+
+    for (i in location) println("${i.first} ${i.second}")
+}
+
+fun mergeSort(arr: Array<Pair<Int, Int>>, temp: Array<Pair<Int, Int>>, start: Int, end: Int) {
+    val mid = (start + end) / 2
+
+    if (start < end) {
+        mergeSort(arr, temp, start, mid)
+        mergeSort(arr, temp, mid+1, end)
+        merge(arr, temp, start, mid+1, end)
+    }
+}
+
+fun merge(arr: Array<Pair<Int, Int>>, temp: Array<Pair<Int, Int>>, start: Int, mid: Int, end: Int) {
+    var tempPos = start
+    var left = start
+    var right = mid
+    while (left <= mid - 1 && right <= end) {
+        if (arr[left].first < arr[right].first) temp[tempPos] = arr[left++]
+        else if (arr[left].first == arr[right].first) {
+            if (arr[left].second < arr[right].second) {
+                temp[tempPos] = arr[left]
+                left++
+            } else {
+                temp[tempPos] = arr[right]
+                right++
+            }
+        } else {
+            temp[tempPos] = arr[right]
+            right++
+        }
+        tempPos++
+    }
+
+    while (left <= mid - 1) temp[tempPos++] = arr[left++]
+
+    while (right <= end) temp[tempPos++] = arr[right++]
+
+    for (i in start..end) arr[i] = temp[i]
+}
