bacbb1edf9370377c72ac375daeef8a717dd063a
beom84 <cjja00@konkuk.ac.kr>
2025-03-04 20:58:18 +0900

    [Silver II] Title: 로또, Time: 128 ms, Memory: 21076 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.kt
+++ 백준/Silver/6603. 로또/로또.kt
@@ -1,5 +1,3 @@
-package baekjoon_practice
-
 lateinit var numbers: IntArray
 lateinit var visited: Array<Boolean>
 
@@ -9,32 +7,30 @@ val sb = StringBuilder()
 fun main() = with(System.`in`.bufferedReader()) {
     while (true) {
         numbers = readLine().split(" ").map { it.toInt() }.toIntArray()
-        totalNum = numbers[0] // 첫 번째 원소는 개수 정보
-
+        totalNum = numbers[0]
+        visited = Array(totalNum + 1) { false }
         if (totalNum == 0) break
 
-        visited = Array(totalNum) { false } // ✅ 불필요한 +1 제거
         printNumber(0, 0)
         sb.append("\n")
     }
-
     print(sb)
 }
 
 fun printNumber(number: Int, count: Int) {
     if (count == 6) {
-        for (i in 0 until totalNum) {
+        for (i in 0 until  totalNum) {
             if (visited[i]) {
-                sb.append(numbers[i + 1]).append(" ") // ✅ numbers[1]부터 숫자가 시작함
+                sb.append(numbers[i+1]).append(" ")
             }
         }
         sb.append("\n")
         return
     }
 
-    for (i in number until totalNum) { // ✅ 범위 수정
+    for (i in number until totalNum) {
         visited[i] = true
-        printNumber(i + 1, count + 1)
+        printNumber(number = i + 1, count = count + 1)
         visited[i] = false
     }
 }
