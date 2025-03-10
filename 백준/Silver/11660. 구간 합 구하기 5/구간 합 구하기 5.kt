7a755a7dd1713187d9ab58399a6cac2d8ca41b60
beom84 <cjja00@konkuk.ac.kr>
2025-03-10 16:11:09 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 660 ms, Memory: 127724 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.kt
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.kt
@@ -1,30 +1,32 @@
-fun main() = with(System.`in`.bufferedReader()) {
-    val (n, m) = readLine().split(" ").map { it.toInt() }
-    val table = Array(n + 1) { IntArray(n + 1) }
-    val sumTable = Array(n + 1) { IntArray(n + 1) }
+import java.util.*
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+
+    var st = StringTokenizer(br.readLine())
+    val n = st.nextToken().toInt()
+    val m = st.nextToken().toInt()
+
+    val sumArr = Array(n + 1) { IntArray(n + 1) }
+
+    repeat(n) { i ->
+        st = StringTokenizer(br.readLine())
+        repeat(n) { j ->
+            val now = st.nextToken().toInt()
+            sumArr[i + 1][j + 1] = sumArr[i + 1][j] + sumArr[i][j + 1] - sumArr[i][j] + now
+        }
+    }
+
     val sb = StringBuilder()
-
-    for (i in 1..table.lastIndex) {
-        val row = readLine().split(" ").map { it.toInt() }
-        for (j in 1..n) {
-            table[i][j] = row[j - 1]
-        }
-    }
-
-    for (i in 1..table.lastIndex) {
-        for (j in 1..table.lastIndex) {
-            sumTable[i][j] = sumTable[i - 1][j] + sumTable[i][j - 1] - sumTable[i - 1][j - 1] + table[i][j]
-        }
-    }
     repeat(m) {
-//        val location = readLine().split(" ").map { it.toInt() }
-//        val a = Pair(location[0], location[1])
-//        val b = Pair(location[2], location[3])
-        val (x1, y1, x2, y2) = readLine().split(" ").map { it.toInt() }
-        val sum =
-            sumTable[x2][y2] - sumTable[x2][y1 - 1] - sumTable[x1- 1][y2] + sumTable[x1 - 1][y1 - 1]
-
-        sb.append(sum).append("\n")
+        st = StringTokenizer(br.readLine())
+        val x1 = st.nextToken().toInt()
+        val y1 = st.nextToken().toInt()
+        val x2 = st.nextToken().toInt()
+        val y2 = st.nextToken().toInt()
+        
+        val r = sumArr[x2][y2] - sumArr[x2][y1 - 1] - sumArr[x1 - 1][y2] + sumArr[x1 - 1][y1 - 1]
+        sb.appendLine(r)
     }
     println(sb)
 }
