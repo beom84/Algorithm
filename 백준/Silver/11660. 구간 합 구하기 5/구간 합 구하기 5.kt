598d24d3d933d851bdc9364c9f6c3973c2841f15
beom84 <cjja00@konkuk.ac.kr>
2025-03-10 15:55:44 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1148 ms, Memory: 193900 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.kt
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.kt
@@ -10,18 +10,19 @@ fun main() = with(System.`in`.bufferedReader()) {
             table[i][j] = row[j - 1]
         }
     }
-    
+
     for (i in 1..table.lastIndex) {
         for (j in 1..table.lastIndex) {
             sumTable[i][j] = sumTable[i - 1][j] + sumTable[i][j - 1] - sumTable[i - 1][j - 1] + table[i][j]
         }
     }
     repeat(m) {
-        val location = readLine().split(" ").map { it.toInt() }
-        val a = Pair(location[0], location[1])
-        val b = Pair(location[2], location[3])
+//        val location = readLine().split(" ").map { it.toInt() }
+//        val a = Pair(location[0], location[1])
+//        val b = Pair(location[2], location[3])
+        val (x1, y1, x2, y2) = readLine().split(" ").map { it.toInt() }
         val sum =
-            sumTable[b.first][b.second] - sumTable[b.first][a.second - 1] - sumTable[a.first - 1][b.second] + sumTable[a.first - 1][a.second - 1]
+            sumTable[x2][y2] - sumTable[x2][y1 - 1] - sumTable[x1- 1][y2] + sumTable[x1 - 1][y1 - 1]
 
         sb.append(sum).append("\n")
     }
