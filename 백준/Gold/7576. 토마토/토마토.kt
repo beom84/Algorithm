3191df41e6ec6055275e4fcd251310d8712efcd8
beom84 <cjja00@konkuk.ac.kr>
2025-03-27 15:04:40 +0900

    [Gold V] Title: 토마토, Time: 892 ms, Memory: 168908 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.kt
+++ 백준/Gold/7576. 토마토/토마토.kt
@@ -1,20 +1,22 @@
+import kotlin.math.max
+
 fun main() = with(System.`in`.bufferedReader()) {
     val (m, n) = readLine().split(" ").map { it.toInt() }
     val board = Array(n) { readLine().split(" ").map { it.toInt() }.toIntArray() }
-    val tomato = mutableListOf<Pair<Int, Int>>()
     val queue = ArrayDeque<Pair<Int, Int>>()
-    var dayCount = 0
-    var todayTomatoNum = 0
+    val distance = Array(n) { IntArray(m) { -1 } }
     var emptySpaceNum = 0
     var totalCount = 0
+    var max = 0
+
     val dx = intArrayOf(0, 1, 0, -1)
     val dy = intArrayOf(1, 0, -1, 0)
+    val boardSize = n * m
 
     for (i in board.indices) {
         for (j in board[0].indices) {
             if (board[i][j] == 1) {
-                tomato.add(i to j)
-                todayTomatoNum++
+                queue.add(i to j)
                 totalCount++
             } else if (board[i][j] == -1) {
                 emptySpaceNum++
@@ -22,32 +24,23 @@ fun main() = with(System.`in`.bufferedReader()) {
         }
     }
 
-    if (totalCount == n * m) {
+    if (totalCount + emptySpaceNum == boardSize) {
         println(0)
         return
     }
 
-    repeat(todayTomatoNum) { idx ->
-        queue.add(tomato[idx])
-    }
-
-    while (todayTomatoNum != 0) {
-        var tomorrowTomatoNum = 0
-        repeat(todayTomatoNum) {
-            val next = queue.removeFirst()
-            for (dir in 0..3) {
-                val x = next.first + dx[dir]
-                val y = next.second + dy[dir]
-                if (x < 0 || x >= n || y < 0 || y >= m) continue
-                if (board[x][y] != 0) continue
-                board[x][y] = 1
-                queue.add(x to y)
-                tomorrowTomatoNum++
-                totalCount++
-            }
+    while (queue.isNotEmpty()) {
+        val next = queue.removeFirst()
+        for (dir in 0..3) {
+            val x = next.first + dx[dir]
+            val y = next.second + dy[dir]
+            if (x < 0 || x >= n || y < 0 || y >= m) continue
+            if (board[x][y] != 0 || distance[x][y] >= 0) continue
+            distance[x][y] = distance[next.first][next.second] + 1
+            max = max(max, distance[x][y])
+            totalCount++
+            queue.add(x to y)
         }
-        todayTomatoNum = tomorrowTomatoNum
-        dayCount++
     }
-    println(if (totalCount + emptySpaceNum != n * m) -1 else dayCount - 1)
+    println(if (totalCount + emptySpaceNum != n * m) -1 else max + 1)
 }
