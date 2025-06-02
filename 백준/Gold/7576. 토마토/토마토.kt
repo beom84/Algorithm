4b2a91130f51a932236013361569271536104add
beom84 <cjja00@konkuk.ac.kr>
2025-06-02 18:24:49 +0900

    [Gold V] Title: 토마토, Time: 604 ms, Memory: 137004 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.kt
+++ 백준/Gold/7576. 토마토/토마토.kt
@@ -1,46 +1,55 @@
-import kotlin.math.max
+import java.util.*
+
+private val bw = System.`out`.bufferedWriter()
+private val board = Array(1000) { IntArray(1000) }
+private val visited = Array(1000) { BooleanArray(1000) }
+private val dx = intArrayOf(1, 0, -1, 0)
+private val dy = intArrayOf(0, 1, 0, -1)
+private var timer = -1
 
 fun main() = with(System.`in`.bufferedReader()) {
-    val (m, n) = readLine().split(" ").map { it.toInt() }
-    val board = Array(n) { readLine().split(" ").map { it.toInt() }.toIntArray() }
+    val (m,n) = readLine().split(" "). map { it.toInt() }
     val queue = ArrayDeque<Pair<Int, Int>>()
-    val distance = Array(n) { IntArray(m) { -1 } }
-    var emptySpaceNum = 0
-    var totalCount = 0
-    var max = 0
 
-    val dx = intArrayOf(0, 1, 0, -1)
-    val dy = intArrayOf(1, 0, -1, 0)
-    val boardSize = n * m
-
-    for (i in board.indices) {
-        for (j in board[0].indices) {
-            if (board[i][j] == 1) {
-                queue.add(i to j)
-                totalCount++
-            } else if (board[i][j] == -1) {
-                emptySpaceNum++
+    repeat(n) { i ->
+        val str = StringTokenizer(readLine())
+        repeat(m) { j ->
+            val riped = str.nextToken().toInt()
+            board[i][j] = riped
+            if (riped == 1) {
+                visited[i][j] = true
+                queue.offer(i to j)
             }
         }
     }
 
-    if (totalCount + emptySpaceNum == boardSize) {
-        println(0)
-        return
+    while (!queue.isEmpty()) {
+        val size = queue.size
+        timer++
+        repeat(size) {
+            val next = queue.poll()
+            for (dir in 0..3) {
+                val nx = next.first + dx[dir]
+                val ny = next.second + dy[dir]
+
+                if (nx !in 0..<n || ny !in 0..<m) continue
+                if (board[nx][ny] != 0 || visited[nx][ny]) continue
+
+                board[nx][ny] = 1
+                queue.offer(nx to ny)
+            }
+        }
+
     }
 
-    while (queue.isNotEmpty()) {
-        val next = queue.removeFirst()
-        for (dir in 0..3) {
-            val x = next.first + dx[dir]
-            val y = next.second + dy[dir]
-            if (x < 0 || x >= n || y < 0 || y >= m) continue
-            if (board[x][y] != 0 || distance[x][y] >= 0) continue
-            distance[x][y] = distance[next.first][next.second] + 1
-            max = max(max, distance[x][y])
-            totalCount++
-            queue.add(x to y)
+    repeat(n) { i ->
+        repeat(m) { j ->
+            if (board[i][j] == 0) {
+                bw.append("-1").flush()
+                return
+            }
         }
     }
-    println(if (totalCount + emptySpaceNum != n * m) -1 else max + 1)
+
+    bw.append("$timer").flush()
 }
