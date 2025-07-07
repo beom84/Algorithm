591d2b15dc808f3983e54dab157abfae162f459a
beom84 <cjja00@konkuk.ac.kr>
2025-07-07 22:03:51 +0900

    [Silver I] Title: 그림, Time: 424 ms, Memory: 54116 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.kt
+++ 백준/Silver/1926. 그림/그림.kt
@@ -1,51 +1,64 @@
 import java.util.*
+import kotlin.math.max
 
-private val bw = System.`out`.bufferedWriter()
-private val board = Array(500) { IntArray(500) }
-private val visited = Array(500) { BooleanArray(500) }
-private val dx = intArrayOf(1, 0, -1, 0)
-private val dy = intArrayOf(0, 1, 0, -1)
+private lateinit var board: Array<IntArray>
+private lateinit var visited: Array<BooleanArray>
+private val dx = intArrayOf(0, 1, 0, -1)
+private val dy = intArrayOf(1, 0, -1, 0)
 
 
 fun main() = with(System.`in`.bufferedReader()) {
     val (n, m) = readLine().split(" ").map { it.toInt() }
-    val queue = ArrayDeque<Pair<Int,Int>>()
-    var max = 0
-    var num = 0
-    repeat(n) { i ->
-        val str = StringTokenizer(readLine())
-        repeat(m) { j ->
-            val painted = str.nextToken().toInt()
-            board[i][j] = painted
+    board = Array(n + 1) { IntArray(m + 1) }
+    visited = Array(n + 1) { BooleanArray(m + 1) }
+    var count = 0
+    var maxExtent = 0
+
+    repeat(n) { x ->
+        val st = StringTokenizer(readLine())
+        repeat(m) { y ->
+            val painted = st.nextToken().toInt()
+            board[x + 1][y + 1] = painted
         }
     }
+    // 1인데 방문하지 않았다면 bfs 실행
 
-    for( i in 0 ..< n){
-        for(j in 0 ..< m){
-            if(board[i][j] == 1 && !visited[i][j]){
-                num++
-                var count = 0
-                visited[i][j] = true
-                queue.offer(i to j)
+    repeat(n){ x ->
+        repeat(m){y ->
+            if(board[x+1][y+1] ==1 && !visited[x+1][y+1]){
+                maxExtent = max(bfs(x+1,y+1,n,m),maxExtent)
                 count++
-
-                while(!queue.isEmpty()){
-                    val next = queue.poll()
-                    for( dir in 0 .. 3){
-                        val x = next.first + dx[dir]
-                        val y = next.second + dy[dir]
-
-                        if(x !in 0..<n || y !in 0 ..< m) continue
-                        if(board[x][y] != 1 || visited[x][y]) continue
-
-                        visited[x][y] = true
-                        queue.offer(x to y)
-                        count++
-                    }
-                }
-                max = max.coerceAtLeast(count)
             }
         }
     }
-    bw.append("$num\n$max").flush()
+
+    println("$count\n$maxExtent")
+}
+
+private fun bfs(x: Int, y: Int, col: Int, row: Int): Int {
+    val location = Pair(x, y)
+    val queue = ArrayDeque<Pair<Int, Int>>() // 큐니까 offer poll
+    var extent = 1
+
+    queue.offer(location)
+    visited[x][y] = true
+
+    while (queue.isNotEmpty()) {
+        val current = queue.poll()
+        val cx = current.first
+        val cy = current.second
+
+        for (i in 0..3) {
+            val nx = cx + dx[i]
+            val ny = cy + dy[i]
+
+            if (nx <= 0 || nx > col || ny <= 0 || ny > row) continue
+            if (board[nx][ny] == 0 || visited[nx][ny]) continue
+
+            queue.offer(nx to ny)
+            visited[nx][ny] = true
+            extent++
+        }
+    }
+    return extent
 }
