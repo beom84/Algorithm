8c2ca8b7edcf18a40a33ebb858f86621fc4d6006
beom84 <cjja00@konkuk.ac.kr>
2025-07-09 10:22:47 +0900

    [Silver I] Title: 미로 탐색, Time: 156 ms, Memory: 22004 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.kt
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.kt
@@ -1,27 +1,47 @@
+import java.util.*
+
+private val dx = intArrayOf(0, 1, 0, -1)
+private val dy = intArrayOf(1, 0, -1, 0)
+
 fun main() = with(System.`in`.bufferedReader()) {
     val (n, m) = readLine().split(" ").map { it.toInt() }
-    val board = Array(n) { readLine().chunked(1).map { it.toInt() }.toIntArray() }
 
-    val queue = ArrayDeque<Pair<Int, Int>>()
-    val visited = Array(n) { IntArray(m) { 0 } }
-    val dx = intArrayOf(1, 0, -1, 0)
-    val dy = intArrayOf(0, 1, 0, -1)
+    val board = Array(n) { IntArray(m) }
+    val visited = Array(n) { BooleanArray(m) }
+    val queue = ArrayDeque<MinLocation>()
 
-    queue.add(0 to 0)
-    visited[0][0] = 1
+    repeat(n) { x ->
+        val str = readLine().chunked(1)
+        repeat(m) { y ->
+            board[x][y] = str[y].toInt()
 
-    while (queue.isNotEmpty()) {
-        val next = queue.removeFirst()
-        val distance = visited[next.first][next.second]
-        for (dir in 0..3) {
-            val x = next.first + dx[dir]
-            val y = next.second + dy[dir]
-            if (x < 0 || x >= n || y < 0 || y >= m) continue
-            if (board[x][y] == 0 || visited[x][y] >= 1) continue
-            queue.add(x to y)
-            visited[x][y] = distance + 1
         }
     }
 
-    println(visited[n - 1][m - 1])
+    queue.add(MinLocation(0, 0, 1))
+    visited[0][0] = true
+
+    while (queue.isNotEmpty()) {
+        val current = queue.poll()
+
+        if(current.x == n-1 && current.y == m-1){
+            println(current.count)
+            return
+        }
+
+        for (i in 0..3) {
+            val nx = current.x + dx[i]
+            val ny = current.y + dy[i]
+
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue
+            if (board[nx][ny] == 0 || visited[nx][ny]) continue
+
+            queue.offer(MinLocation(nx, ny, current.count + 1))
+            visited[nx][ny] = true
+        }
+    }
+
 }
+
+data class MinLocation(val x: Int, val y: Int, val count: Int)
+
