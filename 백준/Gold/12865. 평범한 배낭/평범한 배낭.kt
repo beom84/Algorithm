c0996a64ee21cef236e81f745a23396033f769ae
beom84 <cjja00@konkuk.ac.kr>
2025-09-19 16:28:32 +0900

    [Gold V] Title: 평범한 배낭, Time: 156 ms, Memory: 21916 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.kt
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.kt
@@ -2,28 +2,29 @@ import kotlin.math.max
 
 private fun main() = with(System.`in`.bufferedReader()) {
     val (n, k) = readLine().split(" ").map { it.toInt() }
-    val weights = IntArray(n+1)
-    val values = IntArray(n+1)
+    val weights = IntArray(n)
+    val values = IntArray(n)
 
+    // 굳이 n+1 크기로 만들 필요 없이 0-indexed로 처리
     repeat(n) { i ->
-        val line = readLine().split(" ").map { it.toInt() }
-        weights[i+1] = line[0]
-        values[i+1] = line[1]
+        val (w, v) = readLine().split(" ").map { it.toInt() }
+        weights[i] = w
+        values[i] = v
     }
 
-    val dp = Array(n+1) { IntArray(k+1) }
+    // O(K) 공간만 사용하는 1차원 dp 배열
+    val dp = IntArray(k + 1)
 
-    for (i in 1 .. n) {
+    for (i in 0 until n) {
         val w = weights[i]
         val v = values[i]
 
-        for (j in 1 .. k)
-            if (w > j) {
-                dp[i][j] = dp[i - 1][j]
-            } else {
-                dp[i][j] = max(dp[i - 1][j], v + dp[i - 1][j - w])
-            }
+        // 🚨 안쪽 반복문을 k부터 w까지 거꾸로 순회
+        for (j in k downTo w) {
+            // 현재 물건을 넣는 경우 vs 넣지 않는 경우
+            dp[j] = max(dp[j], v + dp[j - w])
+        }
     }
 
-    println(dp[n][k])
+    println(dp[k])
 }
